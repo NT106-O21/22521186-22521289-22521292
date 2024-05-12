008b7cdffc738afd810cbc8b5f36f03389e16565
VuBaTai2004 <22521289@gm.uit.edu.vn>
2024-05-12 14:10:18 +0700

    install Webview2

--- Lab04/Exercise1/Exercise1/Ex4.Designer.cs
+++ Lab04/Exercise1/Exercise1/Ex4.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Ex4";
+            SuspendLayout();
+            // 
+            // Ex4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Ex4";
+            Text = "Ex4";
+            ResumeLayout(false);
         }
 
         #endregion
